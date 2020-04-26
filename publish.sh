bfc8b5b15737b3046af8af1a2eaffa62de01fd39
Ivo Widjaja <dreamind@gmail.com>
2020-04-26 18:03:29 +1000

    Data update on Sun 26 Apr 2020 18:03:29 AEST

--- publish.sh
+++ publish.sh
@@ -1,12 +1,16 @@
 # make sure working git is clean
 # assume the latest data has been pulled into COVID-19 folder from CSSEGISandData/COVID-19.git
+git checkout master
 node scripts/get-covid-data.js
+git add .
+git commit -m "Data update on $(date)"
+git push origin master
 pnpm run build
 mkdir -p ~/tmp
 cp -R dist/. ~/tmp/covid-nineteen-build
 git checkout gh-pages
 git pull origin gh-pages
-cp -R ~/tmp/covid-nineteen-build ./
+cp -R ~/tmp/covid-nineteen-build/. .
 git add .
-git commit -m 'Published on $(date)'
+git commit -m "Data update on $(date)"
 git push origin gh-pages
